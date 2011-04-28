2afb4eb37a7c382e45d4b97d1548d0dd6999a47c
Florin Bratu <kreaThorEx@gmail.com>
2011-04-29 01:49:18 +0200

    the user feedback dialog is now made based on a layout

--- src/com/killerappz/android/spinlogo/preferences/UserFeedbackDialogPreference.java
+++ src/com/killerappz/android/spinlogo/preferences/UserFeedbackDialogPreference.java
@@ -4,12 +4,7 @@ import android.content.Context;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.killerappz.android.spinlogo.R;
@@ -26,54 +21,29 @@ import com.killerappz.android.spinlogo.R;
  */
 public class UserFeedbackDialogPreference extends DialogPreference {
 	
-	private CheckBox logcatCheckBox; 
-
 	public UserFeedbackDialogPreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		setDialogIcon(android.R.drawable.ic_dialog_info);
+		setDialogLayoutResource(R.layout.user_feedback);
 	}
 	
 	@Override
-	protected View onCreateDialogView() {
-		/*
-        mReportFileName = getIntent().getStringExtra(ErrorReporter.EXTRA_REPORT_FILE_NAME);
-        Log.d(LOG_TAG, "Opening CrashReportDialog for " + mReportFileName);
-        if (mReportFileName == null) {
-            finish();
-        }
-        */
-        
-        // TODO maybe one day... do this via an inflatable layout...
-        final Context ctx = getContext();
-
-        LinearLayout root = new LinearLayout(ctx);
-        root.setOrientation(LinearLayout.VERTICAL);
-        root.setPadding(10, 10, 10, 10);
-        root.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-
-        ScrollView scroll = new ScrollView(ctx);
-        root.addView(scroll, new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT, 1.0f));
-
-        TextView text = new TextView(ctx);
-        text.setText(R.string.user_feedback_text);
-        scroll.addView(text, LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
-
-        TextView label = new TextView(ctx);
-        label.setText(R.string.user_feedback_details);
-        label.setPadding(label.getPaddingLeft(), 10, label.getPaddingRight(), label.getPaddingBottom());
-        root.addView(label, new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-
-        EditText userComment = new EditText(ctx);
-        userComment.setLines(2);
-        root.addView(userComment,
-        		new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-        
-        logcatCheckBox = new CheckBox(ctx);
-        logcatCheckBox.setText(R.string.user_feedback_logcat_checkbox);
-        root.addView(logcatCheckBox,
-        		new LinearLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
-        
-        return root.getRootView();
+	protected void onBindDialogView(View view) {
+		super.onBindDialogView(view);
+		
+		final CheckBox checkbox = (CheckBox) view.findViewById(R.id.logcat);
+		checkbox.setOnClickListener(new View.OnClickListener() {
+			@Override
+		    public void onClick(View v) {
+		        // Perform action on clicks, depending on whether it's now checked
+		        if (((CheckBox) v).isChecked()) {
+		            Toast.makeText(getContext(), "Selected", Toast.LENGTH_SHORT).show();
+		        } else {
+		            Toast.makeText(getContext(), "Not selected", Toast.LENGTH_SHORT).show();
+		        }
+		    }
+		});
+		
 	}
 	
 	@Override
@@ -82,7 +52,7 @@ public class UserFeedbackDialogPreference extends DialogPreference {
 		if(!positiveResult) {
 			Toast.makeText(getContext(), "Thou pressed Cancel" , Toast.LENGTH_SHORT).show();
 		} else {
-			Toast.makeText(getContext(), "Thou pressed OK. Checkbox is " + (logcatCheckBox.isChecked() ? "checked" : "not checked") , Toast.LENGTH_SHORT).show();
+			Toast.makeText(getContext(), "Thou pressed OK" , Toast.LENGTH_SHORT).show();
 		}
 	}
 }
