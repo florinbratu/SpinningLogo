80c5c2af1b3229419eb4e6b38d271888a97c60a9
Florin Bratu <kreaThorEx@gmail.com>
2011-05-10 20:41:42 +0200

    more renaming work from spinlogo to lwp.dukenukem

--- src/com/killerappz/android/lwp/dukenukem/licensing/LicenseValidationDialog.java
+++ src/com/killerappz/android/lwp/dukenukem/licensing/LicenseValidationDialog.java
@@ -1,4 +1,4 @@
-package com.killerappz.android.spinlogo.licensing;
+package com.killerappz.android.lwp.dukenukem.licensing;
 
 import android.app.Activity;
 import android.app.NotificationManager;
@@ -15,9 +15,9 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.killerappz.android.spinlogo.Constants;
-import com.killerappz.android.spinlogo.R;
-import com.killerappz.android.spinlogo.preferences.LicenseStatusPreference;
+import com.killerappz.android.lwp.dukenukem.preferences.LicenseStatusPreference;
+import com.killerappz.android.lwp.dukenukem.Constants;
+import com.killerappz.android.lwp.dukenukem.R;
 
 /**
  * Dialog to be displayed on wallpaper startup
