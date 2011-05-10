c223fae233c17fcdf008ebe63d30ab8c8f3d533e
Florin Bratu <kreaThorEx@gmail.com>
2011-05-10 21:13:05 +0200

    more renaming work from spinlogo to lwp.orthodox

--- src/com/killerappz/android/lwp/orthodox/preferences/LicenseStatusPreference.java
+++ src/com/killerappz/android/lwp/orthodox/preferences/LicenseStatusPreference.java
@@ -1,4 +1,4 @@
-package com.killerappz.android.spinlogo.preferences;
+package com.killerappz.android.lwp.orthodox.preferences;
 
 import android.content.Context;
 import android.content.Intent;
@@ -12,8 +12,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.killerappz.android.spinlogo.Constants;
-import com.killerappz.android.spinlogo.R;
+import com.killerappz.android.lwp.orthodox.Constants;
+import com.killerappz.android.lwp.orthodox.R;
 
 /**
  * Entrance in the Preferences page for 
