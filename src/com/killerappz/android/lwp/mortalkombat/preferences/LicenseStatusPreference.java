b8a49bf2c52a625bbc712e2922b604b088919dca
Florin Bratu <kreaThorEx@gmail.com>
2011-05-10 21:50:04 +0200

    renaming work. merge artefacts fixup. increase version number

--- src/com/killerappz/android/lwp/mortalkombat/preferences/LicenseStatusPreference.java
+++ src/com/killerappz/android/lwp/mortalkombat/preferences/LicenseStatusPreference.java
@@ -1,4 +1,4 @@
-package com.killerappz.android.spinlogo.preferences;
+package com.killerappz.android.lwp.mortalkombat.preferences;
 
 import android.content.Context;
 import android.content.Intent;
@@ -12,8 +12,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.killerappz.android.spinlogo.Constants;
-import com.killerappz.android.spinlogo.R;
+import com.killerappz.android.lwp.mortalkombat.Constants;
+import com.killerappz.android.lwp.mortalkombat.R;
 
 /**
  * Entrance in the Preferences page for 
