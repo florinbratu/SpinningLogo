7619adc5d2432eaefc281c8caccb1b3549273508
Florin Bratu <kreaThorEx@gmail.com>
2011-05-10 20:59:18 +0200

    more renaming work from spinlogo to lwp.superman

--- src/com/killerappz/android/lwp/superman/preferences/LicenseStatusPreference.java
+++ src/com/killerappz/android/lwp/superman/preferences/LicenseStatusPreference.java
@@ -1,4 +1,4 @@
-package com.killerappz.android.spinlogo.preferences;
+package com.killerappz.android.lwp.superman.preferences;
 
 import android.content.Context;
 import android.content.Intent;
@@ -12,8 +12,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.killerappz.android.spinlogo.Constants;
-import com.killerappz.android.spinlogo.R;
+import com.killerappz.android.lwp.superman.Constants;
+import com.killerappz.android.lwp.superman.R;
 
 /**
  * Entrance in the Preferences page for 
