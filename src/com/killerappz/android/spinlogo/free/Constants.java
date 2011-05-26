ae5329b7a1e8c58dfd94cc55c7bcf83ac5e3cbe0
Florin Bratu <kreaThorEx@gmail.com>
2011-05-26 19:21:46 +0200

    prefs name change

--- src/com/killerappz/android/spinlogo/free/Constants.java
+++ src/com/killerappz/android/spinlogo/free/Constants.java
@@ -1,7 +1,7 @@
 package com.killerappz.android.spinlogo.free;
 
 public interface Constants {
-	public static final String PREFS_NAME	= "spinlogo_settings";
+	public static final String PREFS_NAME	= "spinlogo_free_settings";
 	public static final float ROTATION_SPEED_UNIT = 0.1f;
 	public static final int DEFAULT_ROTATION_SPEED = 25;
 	// the file containing the model of the logo
