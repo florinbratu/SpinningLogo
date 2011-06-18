646012a1a7fdc967ed8569749a4cd3c2e74e7808
Florin Bratu <kreaThorEx@gmail.com>
2011-06-18 19:31:37 +0200

    bugfix - decouple rotation speed from revolution speed

---
+++ src/com/killerappz/android/spinlogo/preferences/RevolutionSpeedPreference.java
@@ -0,0 +1,23 @@
+package com.killerappz.android.spinlogo.preferences;
+
+import com.killerappz.android.spinlogo.Constants;
+import com.killerappz.android.spinlogo.R;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Control revolution speed
+ * @author florin
+ *
+ */
+public class RevolutionSpeedPreference extends SeekBarPreference {
+
+	public RevolutionSpeedPreference(Context context, AttributeSet attrs) {
+		super(context, attrs, 
+				Integer.parseInt(context.getString(R.string.revolution_speed_default_value)), 
+				Integer.parseInt(context.getString(R.string.revolution_speed_max_value)), 
+				Constants.REVOLUTION_SPEED_UNIT);
+	}
+
+}
