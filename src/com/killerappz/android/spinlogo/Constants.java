2425936e55c328acb5221ed1e35a1b4ad958b378
Florin Bratu <kreaThorEx@gmail.com>
2011-06-25 23:30:35 +0200

    transformers logo model

--- src/com/killerappz/android/spinlogo/Constants.java
+++ src/com/killerappz/android/spinlogo/Constants.java
@@ -5,7 +5,7 @@ public interface Constants {
 	public static final float ROTATION_SPEED_UNIT = 0.1f;
 	public static final int DEFAULT_ROTATION_SPEED = 25;
 	// the file containing the model of the logo
-	public static final String LOGO_MODEL_FILE = "raw/ying_yang_obj";
+	public static final String LOGO_MODEL_FILE = "raw/logo_obj";
 	// the folder under res/ where we find textures. Android internal cuisine
 	public static final String TEXTURES_LOCATION = "drawable/";
 	
