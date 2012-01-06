412f7a9d436bbb937a81c08660f50212e0056685
Florin Bratu <kreaThorEx@gmail.com>
2012-01-06 08:10:34 +0100

    change default texture

--- src/com/killerappz/android/spinlogo/Constants.java
+++ src/com/killerappz/android/spinlogo/Constants.java
@@ -34,7 +34,7 @@ public interface Constants {
 	public static final String DEFAULT_LOGO_TEXTURE_NAME = "texture_black_grey";
 	// skybox texture
 	public static final String SKYBOX_TEXTURE_KEY = "skyboxTexture";
-	public static final String DEFAULT_SKYBOX_TEXTURE_NAME = "skybox_awisdom";
+	public static final String DEFAULT_SKYBOX_TEXTURE_NAME = "skybox_virtual";
 
 	// scaling
 	public static final String SCALING_FACTOR_KEY = "scale";
--- src/com/killerappz/android/spinlogo/SpinningLogo.java
+++ src/com/killerappz/android/spinlogo/SpinningLogo.java
@@ -122,11 +122,11 @@ public class SpinningLogo {
 	private SkyBox createSkyBox() {
 		SkyBox skyBox = new SkyBox(context, textureManager, Constants.SKYBOX_SIZE, Constants.SKYBOX_QUALITY_FACTOR);
 		/* textures */
-		skyBox.addTexture(SkyBox.Face.East,  R.drawable.skybox_awisdom_right,  "east_texture");
-		skyBox.addTexture(SkyBox.Face.South, R.drawable.skybox_awisdom_center, "south_texture");
-		skyBox.addTexture(SkyBox.Face.West,  R.drawable.skybox_awisdom_left,  "west_texture");
-		skyBox.addTexture(SkyBox.Face.Up,    R.drawable.skybox_awisdom_up,    "up_texture");
-		skyBox.addTexture(SkyBox.Face.Down,  R.drawable.skybox_awisdom_down,  "down_texture");
+		skyBox.addTexture(SkyBox.Face.East,  R.drawable.skybox_virtual_right,  "east_texture");
+		skyBox.addTexture(SkyBox.Face.South, R.drawable.skybox_virtual_center, "south_texture");
+		skyBox.addTexture(SkyBox.Face.West,  R.drawable.skybox_virtual_left,  "west_texture");
+		skyBox.addTexture(SkyBox.Face.Up,    R.drawable.skybox_virtual_up,    "up_texture");
+		skyBox.addTexture(SkyBox.Face.Down,  R.drawable.skybox_virtual_down,  "down_texture");
 		return skyBox;
 	}
 
